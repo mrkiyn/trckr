0da3b85a945463024c869fa9aa5b1af9bf3cbb28
mrkiyn <amado.markian@gmail.com>
2024-03-14 15:34:39 +0800

    update the remaining controllers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,18 +3,14 @@ class CategoriesController < ApplicationController
   def index
     @total_no_of_categories = current_user.categories.count # For Header
     @current_date = Date.today.strftime("%Y-%m-%d") # Current date to check the count of pending tasks that is > current date
-    @category = Category.new # For Creation
-    @categories = Task.find_by_sql("
-      SELECT categories.*, 
-        COUNT(tasks.id) FILTER (WHERE tasks.status = 'Pending') AS pending_task_count,
-        COUNT(tasks.id) FILTER (WHERE tasks.due_date < '#{@current_date}' AND tasks.status = 'Pending') AS overdue_task_count,
-        COUNT(tasks.id) FILTER (WHERE tasks.priority_level = 'High' AND tasks.status = 'Pending') AS high_priority_task_count
-      FROM categories
-      LEFT JOIN tasks ON categories.id = tasks.category_id
-      WHERE categories.user_id = '#{current_user.id}'
-      GROUP BY categories.id
-      ORDER BY categories.updated_at DESC;
-    ")
+    @category = current_user.categories.new # For Creation
+    @categories = current_user.categories.select("categories.*, 
+                                              COUNT(tasks.id) FILTER (WHERE tasks.status = 'Pending') AS pending_task_count,
+                                              COUNT(tasks.id) FILTER (WHERE tasks.due_date < '#{@current_date}' AND tasks.status = 'Pending') AS overdue_task_count,
+                                              COUNT(tasks.id) FILTER (WHERE tasks.priority_level = 'High' AND tasks.status = 'Pending') AS high_priority_task_count")
+                                          .left_joins(:tasks)
+                                          .group("categories.id, categories.name, categories.updated_at")
+                                          .order("categories.updated_at DESC")
   end
 
 
@@ -45,13 +41,8 @@ class CategoriesController < ApplicationController
     @tasks_count = @category.tasks.count
 
     @current_date = Date.today
-    @tasks = Task.find_by_sql("
-      SELECT *, TO_CHAR(tasks.due_date, 'MM-DD-YYYY') AS formatted_due_date
-      FROM tasks
-      WHERE category_id = #{@category.id}
-      ORDER BY status DESC, formatted_due_date
-    ")
-
+    @tasks = current_user.tasks.select("*, TO_CHAR(tasks.due_date, 'MM-DD-YYYY') AS formatted_due_date")
+                               .order("status DESC, formatted_due_date")
     @task = @category.tasks.build  
   end
 
